81420e2bdd83eb9a9baef9e249131a1005c71ea2
harshitHash2 <harshit.chauhan2015@gmail.com>
2024-05-23 17:23:04 +0530

    first-set-urls

---
+++ blog/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.starting_page, name="starting-page"),
+    path("posts/", views.posts, name="all-posts"),
+    path("posts/<slug:slug>", views.post_detail, name="post-detail")
+]
+
+
---
+++ blog/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def starting_page():
+    pass
+
+def posts() :
+    pass
+
+def post_detail() :
+    pass
