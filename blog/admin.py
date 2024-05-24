b64913799ce90442dad294f36a3e7d6174b3b67a
harshitHash2 <harshit.chauhan2015@gmail.com>
2024-05-24 14:04:51 +0530

    basics_all

--- blog/admin.py
+++ blog/admin.py
@@ -5,12 +5,12 @@ from .models import Tag, Author, Post
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
-    prepopulated_fields= {"slug": ("title")}
+    prepopulated_fields= {"slug": ("title",)}
     list_filter = ("authors", "tag", "date")
-    list_display= ("title","tag", "authors")
+    list_display= ("title","date", "authors")
 
 admin.site.register(Tag)
 admin.site.register(Author)
-admin.site.register(Post)
+admin.site.register(Post, PostAdmin)
 
 
--- blog/views.py
+++ blog/views.py
@@ -1,12 +1,24 @@
 from django.shortcuts import render
+from .models import  Post
 
 # Create your views here.
 
 def starting_page(request):
-    return render(request, "blog/index.html")
+    latest_post= Post.objects.all().order_by("-date")[:3]
+
+    return render(request, "blog/index.html", {
+        "posts": latest_post,
+    })
 
 def posts(request) :
-    return render(request, "blog/allposts.html")
+    all_p = Post.objects.all().order_by("-date")
+    return render(request, "blog/allposts.html", {
+        "all_p": all_p,
+    })
 
 def post_detail(request, slug) :
-    return render(request, "blog/post_detail.html")
+    one= Post.objects.get(slug=slug)
+    return render(request, "blog/post_detail.html", {
+        "post_d": one
+
+    })
